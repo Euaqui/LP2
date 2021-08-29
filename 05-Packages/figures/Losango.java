98d1263bfb72d285db7cafac90927cf2a3e0f7e0
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-08-29 18:57:29 -0300

    Update Losango.java

--- 05-Packages/figures/Losango.java
+++ 05-Packages/figures/Losango.java
@@ -3,8 +3,8 @@ package figures;
 import java.awt.*;
 
 public class Losango {
-    public int x1,x2,x3,x4, y1,y2,y3,y4;
-    int w, h;
+    private int x1,x2,x3,x4, y1,y2,y3,y4;
+    private int w, h;
 
     public Losango (int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4){
 	this.x1 = x1;
