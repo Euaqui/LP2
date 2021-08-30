36a4d37e6ea312dd1bc671ec4dc3057858daa00c
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-08-30 12:04:55 -0300

    Update Losango.java

--- 05-Packages/figures/Losango.java
+++ 05-Packages/figures/Losango.java
@@ -6,7 +6,7 @@ public class Losango {
     private int x1,x2,x3,x4, y1,y2,y3,y4;
     private int w, h;
 
-    public Losango (int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4){
+    public Losango (int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, int a, int b, int c, int a1, int b1, int c1){
 	this.x1 = x1;
 	this.x2 = x2;
 	this.x3 = x3;
@@ -15,6 +15,12 @@ public class Losango {
         this.y2 = y2;
         this.y3 = y3;
 	this.y4 = y4;
+	this.a = a;
+	this.b = b;
+	this.c = c;
+	this.a1 = a1;
+	this.b1 = b1;
+	this.c1 = c1;
 	this.w = this.y3 - this.y1;
 	this.h = this.y2 - this.y4;
     }
@@ -25,9 +31,9 @@ public class Losango {
     }
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	g.setColor(new Color(0,245,255));
+	g.setColor(new Color(this.a,this.b,this.c));
 	g2d.fillPolygon(new int[] {this.x1, this.x2, this.x3, this.x4}, new int[] {this.y1, this.y2, this.y3, this.y4}, 4);
-	g.setColor(new Color(255,0,0));
+	g.setColor(new Color(this.a1,this.b1,this.c1));
 	g2d.drawPolygon(new int[] {this.x1, this.x2, this.x3, this.x4}, new int[] {this.y1, this.y2, this.y3, this.y4}, 4);
     }
 
