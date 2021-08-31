2de9f92d90f6365feed7c483c609ed1366102080
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-08-31 17:52:46 -0300

    Update 2,1.java

--- 04-Paint/2,1.java
+++ 04-Paint/2,1.java
@@ -27,7 +27,7 @@ class PaintFrame extends JFrame {
         this.setSize(350, 350);
         this.r1 = new Rect(50,50, 100,30, 139,0,139, 255,255,0);
 	this.r2 = new Rect(100,150, 50,150, 0,245,255, 255,0,0);
-	this.r3 = new Rect(150,300, 150,150, 255,215,0, );
+	this.r3 = new Rect(150,300, 150,150, 255,215,0, 0,255,0);
     }
 
     public void paint (Graphics g) {
@@ -66,12 +66,6 @@ class Rect {
 	g2d.fillRect(this.x,this.y, this.w,this.h);
 	g.setColor(new Color(this.a1,this.b1,this.c1));
         g2d.drawRect(this.x,this.y, this.w,this.h);
-	g.setColor(new Color(0,245,255));
-	g2d.fillRect(100,150, 50,150);
-	g.setColor(new Color(255,0,0));
-	g2d.fillRect(150,300, 150,150);
-	g.setColor(new Color(255,215,0));
-	g2d.fillRect(200,40, 50,50);
 	
     }
 }
