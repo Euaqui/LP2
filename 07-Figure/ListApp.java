8835afcb67669c609b0629f03b9dd3358c71bf72
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-09-07 01:15:56 -0300

    Update ListApp.java

--- 07-Figure/ListApp.java
+++ 07-Figure/ListApp.java
@@ -1,3 +1,6 @@
+//Segue arquivo ".PNG"
+//https://user-images.githubusercontent.com/88299200/132283153-4c004896-a984-4048-a058-8ef5b27596f4.png
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,7 +9,7 @@ import java.util.Random;
 
 import figures.*;
 
-class ListApp {
+class ListApp3 {
     public static void main (String[] args) {
         ListFrame frame = new ListFrame();
         frame.setVisible(true);
@@ -30,7 +33,11 @@ class ListFrame extends JFrame {
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
                     int x = rand.nextInt(350);
+		            int x2 = rand.nextInt(350);
+		            int x3 = rand.nextInt(350);
                     int y = rand.nextInt(350);
+		            int y2 = rand.nextInt(350);
+		            int y3 = rand.nextInt(350);
                     int w = rand.nextInt(50);
                     int h = rand.nextInt(50);
                     if (evt.getKeyChar() == 'r') {
@@ -38,7 +45,11 @@ class ListFrame extends JFrame {
                         figs.add(r);
                     } else if (evt.getKeyChar() == 'e') {
                         figs.add(new Ellipse(x,y, w,h));
-                    }
+                    } else if (evt.getKeyChar() == 't') {
+                        figs.add(new Triangle(x,x2,x3, y,y2,y3));
+		            } else if (evt.getKeyChar() == 'l') {
+                        figs.add(new Line(x,y, x2,y2));
+		    }
                     repaint();
                 }
             }
