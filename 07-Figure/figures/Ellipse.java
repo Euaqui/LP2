ba98d7c1ac1b4a9b77f79a658b2000215a90950b
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-09-21 10:36:36 -0300

    Update Ellipse.java

--- 07-Figure/figures/Ellipse.java
+++ 07-Figure/figures/Ellipse.java
@@ -20,6 +20,7 @@ public class Ellipse extends Figure {
     }
 
     public void paint (Graphics g) {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
