fcda8f1b785afc438bfa775602b51ff2b7051c1c
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-09-21 12:15:11 -0300

    Update Figure.java

--- 07-Figure/6.3/figures/Figure.java
+++ 07-Figure/6.3/figures/Figure.java
@@ -3,7 +3,7 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
-    int x, x2, x3, y, y2, y3;
+    int x, x2, y, y2;
     int w, h; 
     public abstract void paint (Graphics g);
 }
