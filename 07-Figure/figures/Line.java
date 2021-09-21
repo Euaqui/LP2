1c6c17efbfd27be182aa64534adc194e7a2074e3
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-09-21 10:36:55 -0300

    Update Line.java

--- 07-Figure/figures/Line.java
+++ 07-Figure/figures/Line.java
@@ -19,6 +19,7 @@ falta implementar
     }
 */
     public void paint (Graphics g) {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawLine(this.x,this.y, this.x1,this.y1);
     }
