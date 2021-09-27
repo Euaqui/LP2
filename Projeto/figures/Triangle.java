abc2c47e0d658d0f81bcbd845be886605721b731
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-09-26 23:47:56 -0300

    Update Triangle.java

--- Projeto/figures/Triangle.java
+++ Projeto/figures/Triangle.java
@@ -3,29 +3,24 @@ package figures;
 import java.awt.*;
 
 public class Triangle extends Figure {
-    private int x1,x2,x3, y1,y2,y3;
-    private int w, h;
+    
 
-    public Triangle(int x1, int x2, int x3, int y1, int y2, int y3){
-	this.x1 = x1;
-	this.x2 = x2;
-	this.x3 = x3;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
-	this.w = this.y3 - this.y1;
-	this.h = y2-((this.y3 + this.y1)/2);
+    public Triangle(int x, int y, int w, int h, Color fundo, Color borda){
+	super(x,y,w,h,fundo,borda);
+	
     }
     public void print () {
         System.out.format("Triangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x1, this.y1);
+            this.w, this.h, this.x, this.y);
     }
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	//g.setColor(new Color(139,0,139));
-	//g2d.fillPolygon(new int[] {this.x1, this.x2, this.x3}, new int[] {this.y1, this.y2, this.y3}, 3);
-	//g.setColor(new Color(255,255,0));
-        g2d.drawPolygon(new int[] {this.x1, this.x2, this.x3}, new int[] {this.y1, this.y2, this.y3}, 3);
+	int x1[] = {this.x, ((this.w) / 2) + (this.x), (this.w) + (this.x)};
+        int y1[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
+	g2d.setColor(this.fundo);
+	g2d.fillPolygon(x1, y1, 3);
+	g2d.setColor(this.borda);
+        g2d.drawPolygon(x1, y1, 3);
     }
 
 }
