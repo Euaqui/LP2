f92f1e82ab3cdfa6377712d3f67e259556e47046
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-10-07 00:43:30 -0300

    Update interfaces.java

--- 09-Interfaces/interfaces.java
+++ 09-Interfaces/interfaces.java
@@ -1,8 +1,12 @@
-interface adjetivo1{
-  metodo 1 (parametros ou retorno);
-  metodo 2();
+interface buyable /*compravel*/{ // objetos: t-shirt, car, table (camisa, carro, mesa)
+  int purchase /*comprar*//*indica se o obeto esta disponivel para compra e o preco*/ (boolean offer,double price);/* oferta indica se algo esta disponivel para compra ou nao,
+                                                                                                         preco indica o preco de compra do objeto*/ 
+  int sell/*vender*//*indica se o objeto está disponivel para venda, o preco*/(boolean offer, double price); /* oferta indica se algo esta disponivel para venda ou nao, 
+                                                                                                      preco indica preco de venda do objeto*/
 }
 
-class objeto 1 implements adjetivo 1
-class objeto 2
-class objeto 3
+
+interface cleanable{ //limpavel objetos: picture frame, cutlery, book (porta-retrato, copo, livro)
+  void to_clean /*limpar*//*indica a quanto temp um objeto nao e limpo*/(int time); // tempo indica a quanto tempo nao sao limpos 
+  void organize /*organizar*//*indica a melhor ordem para organizar os objetos*/ (int order); // ordem indica a ordem para organizar os objetos
+}
