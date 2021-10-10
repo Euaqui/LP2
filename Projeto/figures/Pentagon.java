81fd9cae4397e9b8a1718d3f592dc60cd6915ef4
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-10-10 18:46:30 -0300

    Update Pentagon.java

--- Projeto/figures/Pentagon.java
+++ Projeto/figures/Pentagon.java
@@ -10,8 +10,8 @@ public class Pentagon extends Figure{
 
 	public void paint (Graphics g) {
         	Graphics2D g2d = (Graphics2D) g;
-        	int x1[] = {this.x+10,this.x+25,this.x+65,this.x+80,this.x+43};
-        	int y1[] = {this.y,this.y+30,this.y+30,this.y,this.y-24,};
+        	int x1[] = {this.x+(w/2),this.x-(w/2)+(w/2),this.x-(w/4)+(w/2),this.x+(w/4)+(w/2),this.x+(w/2)+(w/2)};
+        	int y1[] = {this.y,this.y + (h/2),this.y+h ,this.y+h,this.y+(h/2)};
         	g2d.setColor(this.fundo);
         	g2d.fillPolygon(x1, y1, 5); 
         	g2d.setColor(this.borda);
