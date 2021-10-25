e430890895c23be4e669136cf083448a65856a33
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-10-25 01:30:02 -0300

    Update Rect.java

--- Projeto/teste/Rect.java
+++ Projeto/teste/Rect.java
@@ -14,18 +14,11 @@ public class Rect extends Figure{
             this.w, this.h, this.x, this.y);
     }
 
-    public Color contorno = new Color(0,0,0);
-    public void paint (Graphics g, boolean emFoco) {
+    public void paint (Graphics g,boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-
 	g2d.setColor(this.fundo);
         g2d.fillRect(this.x,this.y, this.w,this.h);
 	g2d.setColor(this.borda);
 	g2d.drawRect(this.x,this.y, this.w,this.h);
-
-	if (emFoco){
-		g2d.setColor(contorno);
-        	g2d.drawRect(this.x-4,this.y-4,this.w+8,this.h+8);
-	}
     }
 }
