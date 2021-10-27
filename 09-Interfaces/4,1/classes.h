9aa77eba203b2aee69e0990f743276f8b8ad8fd2
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-10-27 00:57:09 -0300

    Update and rename 09-Interfaces/4,1.java to 09-Interfaces/4,1/classes.h

---
+++ 09-Interfaces/4,1/classes.h
@@ -0,0 +1,25 @@
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
+
+class Base1{
+  public:
+    void impBase1();
+};
+
+void Base1::impBase1(){
+  std::cout<<"Imp classe Base 1"<<std::end1;
+}
+
+class Base2{
+  public:
+    void impBase2();
+};
+
+void Base2::impBase2(){
+  std::cout<<"Imp classe Base 2"<<std::end1;
+}
+
+class CFB: public Base1, public Base2{
+};
+
+#endif
