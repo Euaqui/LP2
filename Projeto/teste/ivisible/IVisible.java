562ce5d71b4b4f275604eac10ffaa27e74e39b5b
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-06 01:19:12 -0300

    Update IVisible.java

--- Projeto/teste/ivisible/IVisible.java
+++ Projeto/teste/ivisible/IVisible.java
@@ -1,1 +1,8 @@
+package ivisible;
 
+import java.awt.Graphics;
+
+public interface IVisible {
+    public boolean clicked (int coordX, int coordY);
+    public void paint (Graphics g, boolean focused);
+}
