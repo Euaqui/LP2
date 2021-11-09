eeab35382e91abc90570c36414aade899a4c40bb
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-08 22:35:56 -0300

    Update LinkedStack.java

--- 11-Stackable/LinkedStack.java
+++ 11-Stackable/LinkedStack.java
@@ -1,10 +1,10 @@
 import java.util.LinkedList;
 
 public class LinkedStack implements IStackable{
-  private LinkedList<Integer> s1 = new LinkedList<Integer>();
+  	private LinkedList<Integer> s1 = new LinkedList<Integer>();
 	
-  int sz1 = 0;
-  int v1;
+  	int sz1 = 0;
+  	int v1;
 
 	public void push(int v1) {
 		s1.addFirst(v1);
@@ -17,7 +17,7 @@ public class LinkedStack implements IStackable{
 		return s1.removeFirst();	
 	}
   
-  public int size() {
+ 	public int size() {
 		return sz1;
 	}
 
