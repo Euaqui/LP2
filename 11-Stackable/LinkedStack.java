53c0659fbfa23700cf8e5129864642b22763a8b5
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-08 22:11:16 -0300

    Create LinkedStack.java

---
+++ 11-Stackable/LinkedStack.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class LinkedStack implements IStackable{
+  private LinkedList<Integer> s1 = new LinkedList<Integer>();
+	
+  int sz1 = 0;
+  
+	public void push(int v1) {
+		s1.addFirst(v);
+    sz1++;
+	}
+
+	
+	public int pop() {
+		s1.removeFirst();
+    sz1--;
+	}
+  
+  public int size() {
+		return sz1;
+	}
+
+}
