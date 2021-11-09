6faf712e6852803b91460218baf9221fbe083c5a
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-08 22:05:52 -0300

    Update ArrayStack.java

--- 11-Stackable/ArrayStack.java
+++ 11-Stackable/ArrayStack.java
@@ -3,22 +3,22 @@ import java.util.ArrayList;
 public class ArrayStack implements IStackable{
 	private ArrayList<Integer> s2 = new ArrayList<Integer>();
 	
-	@Override
+	int sz2 = 0;
+	
 	public void push(int v2) {
 		s2.add(v2);
-    sz2++;
+    		sz2++;
 	}
 
-	@Override
+	
 	public int pop() {
 		s2.remove(v2);
-    
+    		sz2--;
 	}
   
-  @Override
+  
 	public int size() {
-		return ;
+		return sz;
 	}
-
 
 }
