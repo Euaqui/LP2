a7bc059a85ff9884e441d218710231b5cd4a489b
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-15 00:11:47 -0300

    Add files via upload

---
+++ Projeto/Projeto_fase2/Button_Clear.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import figures.*;
+
+public class Button_Clear extends Button{
+
+	public Button_Clear(int idx, Figure fig) {
+		super(idx, fig);
+	}
+	
+	public void paint(Graphics g, boolean focused) {
+        Graphics2D g2d = (Graphics2D) g;
+
+    	if(focused == true) {
+    		g2d.setColor(Color.GRAY);
+    	}
+    	else {
+    		g2d.setColor(Color.WHITE);
+    	}
+        
+        g2d.fillRect(fig.x-6, fig.y-6, fig.w+12, fig.h+12);
+        g2d.setColor(Color.BLACK);
+        
+        g2d.drawRect(fig.x-7, fig.y-7, fig.w+13, fig.h+13);
+        
+        int fontSize = 18;
+        g2d.setFont(new Font("Arial", Font.PLAIN, fontSize));
+        g2d.setColor(Color.red);
+        
+        g2d.drawString("Cls", fig.x-3, fig.y +15);
+	}
+}
---
+++ Projeto/Projeto_fase2/figures/Triangle.java
@@ -0,0 +1,26 @@
+package figures;
+
+import java.awt.*;
+
+public class Triangle extends Figure {
+    
+
+    public Triangle(int x, int y, int w, int h, Color fundo, Color borda){
+	super(x,y,w,h,fundo,borda);
+	
+    }
+    public void print () {
+        System.out.format("Triangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+    public void paint (Graphics g, boolean focused) {
+        Graphics2D g2d = (Graphics2D) g;
+	int x1[] = {this.x, ((this.w) / 2) + (this.x), (this.w) + (this.x)};
+        int y1[] = {this.y + h, (this.y + h) - (this.h), this.y + h};
+	g2d.setColor(this.fundo);
+	g2d.fillPolygon(x1, y1, 3);
+	g2d.setColor(this.borda);
+        g2d.drawPolygon(x1, y1, 3);
+    }
+
+}
