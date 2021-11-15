f71d384a695a6453bf0a35fcd98c4c8586c70b3d
viniciusmrcosta <88299200+Euaqui@users.noreply.github.com>
2021-11-15 00:12:10 -0300

    Delete Projeto/teste directory

--- Projeto/teste/ivisible/IVisible.java
+++
@@ -1,8 +0,0 @@
-package ivisible;
-
-import java.awt.Graphics;
-
-public interface IVisible {
-    public boolean clicked (int coordX, int coordY);
-    public void paint (Graphics g, boolean focused);
-}
